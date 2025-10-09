3f5b449de56d739da75b4339e4cb607c25cf7b31
handabao <18862963156@163.com>
2025-10-09 11:38:38 +0800

    32限制栈中创建对象和调用delete销毁对象

---
+++ src/memory_limit_construction/src/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+
+/// 指针与面向对象
+///  限制栈中创建对象，限制调用delete销毁对象
+class TestMem
+{
+public:
+    static TestMem* Create()
+    {
+        std::cout << "static Create " << std::endl;
+        return new TestMem();
+    }
+    static void Drop(TestMem* tm)
+    {
+        std::cout << "static Drop" << std::endl;
+        delete tm;
+    }
+
+protected:
+    TestMem()
+    {
+        std::cout << "Create TestMem" << std::endl;
+    }
+    virtual ~TestMem()
+    {
+        std::cout << "Drop TestMem" << std::endl;
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    std::cout << "cppds.com" << std::endl;
+
+    {
+        ///  限制栈中创建对象，限制调用delete销毁对象
+        // TestMem tm1;
+        // auto tm2 = new TestMem;
+        TestMem* tm3 = TestMem::Create();
+    }
+    getchar();
+    return 0;
+}
