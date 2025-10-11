9d74c37e8052aab919f3b4fa769ba99abace73d4
handabao <18862963156@163.com>
2025-10-11 11:05:41 +0800

    36虚函数表指针直接访问函数的代码实验

--- src/memory_virtual_function/src/main.cpp
+++ src/memory_virtual_function/src/main.cpp
@@ -12,6 +12,8 @@ public:
     {
         std::cout << "B1 test2" << std::endl;
     }
+
+    int b1 = 100;
 };
 class B2
 {
@@ -25,6 +27,7 @@ public:
     {
         std::cout << "B2 test4" << std::endl;
     }
+    int b2 = 200;
 };
 
 class A : public B1
@@ -78,7 +81,23 @@ public:
     {
         std::cout << "D test4" << std::endl;
     }
+    int a = 100;
 };
+
+
+template <typename dst_type, typename src_type>
+static dst_type union_cast(src_type src)
+{
+    union
+    {
+        src_type s;
+        dst_type d;
+    } u;
+    u.s = src;
+    return u.d;
+}
+
+typedef void (**vptr)(); /// 指向虚指针数组的指针
 
 int main(int argc, char *argv[])
 {
@@ -99,17 +118,71 @@ int main(int argc, char *argv[])
         TestClass(&c);
     }
     std::cout << "============================" << std::endl;
+    /// 多继承各个虚表的获取
     {
-        typedef void (**vptr)();/// 指向虚指针数组的指针
         D d1;
+        std::cout << "&d1 = " << reinterpret_cast<unsigned long long>(&d1) << std::endl;
+        vptr *vptr1 = reinterpret_cast<vptr *>(&d1); ///
+        std::cout << "&(d1.b1) = " << reinterpret_cast<unsigned long long>(&d1.b1)
+                  << std::endl; /// 首地址 + 8， 虚表指针就是8字节的 所以加8
+        std::cout << "vptr1 = " << reinterpret_cast<unsigned long long>(vptr1) << std::endl; /// 首地址
 
-        vptr *vptr1 = (vptr *)(&d1);
-        vptr *vptr2 = (vptr *)(&d1) + 1;
-        (**vptr1)();
-        (*(*vptr1 + 1))();
+        std::cout << "&D::Test1 = " << union_cast<unsigned long long>(&D::Test1) << std::endl;
+        std::cout << "&D::Test2 = " << union_cast<unsigned long long>(&D::Test2) << std::endl;
+
+        auto test1 = **vptr1;
+        test1();
+        std::cout << " test1 = " << reinterpret_cast<unsigned long long>(test1) << std::endl;
+
+        auto test2 = *(*vptr1 + 1);
+        test2();
+        std::cout << " test2 = " << reinterpret_cast<unsigned long long>(test2) << std::endl;
+
+        vptr *vptr2 = reinterpret_cast<vptr *>(&d1) + 2; /// 中间隔开了一个变量 8B一格
+
+        std::cout << "vptr2 = " << reinterpret_cast<unsigned long long>(vptr2)
+                  << std::endl; /// B2的虚表指针 /// 内存对齐 + 8
+
+        std::cout << "&D::test3 = " << union_cast<unsigned long long>(&D::test3) << std::endl;
+        std::cout << "&D::test4 = " << union_cast<unsigned long long>(&D::test4) << std::endl;
+        /// 实际函数地址与 虚表里面的指针不一致
 
         (**vptr2)();
         (*(*vptr2 + 1))();
+
+        std::cout << "&(d1.b2) = " << reinterpret_cast<unsigned long long>(&d1.b2) << std::endl;
+
+        std::cout << "end = " << reinterpret_cast<unsigned long long>(&d1.b2) + 8 << std::endl;
+
+        /// + 8 多一个变量
+
+        std::cout << "sizeof(D) = " << sizeof(D) << std::endl; /// 40
+
+        std::cout << "============================" << std::endl;
+        /// 父类与子类的虚表不一致
+        B1    b1     = static_cast<B1>(d1);
+        vptr *vptrB1 = reinterpret_cast<vptr *>(&b1);
+        std::cout << "vptrB1 = " << reinterpret_cast<unsigned long long>(vptrB1) << std::endl;
+        (**vptrB1)();
+        (*(*vptrB1 + 1))();
+
+        B2    b2     = static_cast<B2>(d1);
+        vptr *vptrB2 = reinterpret_cast<vptr *>(&b2);
+        std::cout << "vptrB2 = " << reinterpret_cast<unsigned long long>(vptrB2) << std::endl;
+        (**vptrB2)();
+        (*(*vptrB2 + 1))();
+    }
+    std::cout << "============================" << std::endl;
+    {
+        A     a1;
+        A     a2;
+        vptr *vptrA1 = reinterpret_cast<vptr *>(&a1); /// 指向虚表(指针数组)的指针(三级指针)
+        vptr *vptrA2 = reinterpret_cast<vptr *>(&a2);
+        std::cout << "vptrA1 = " << reinterpret_cast<unsigned long long>(vptrA1) << std::endl; /// +48 中间父子都有变量
+        std::cout << "vptrA2 = " << reinterpret_cast<unsigned long long>(vptrA2) << std::endl;
+        std::cout << "*vptrA1 = " << reinterpret_cast<unsigned long long>(*vptrA1) << std::endl;
+        std::cout << "*vptrA2 = " << reinterpret_cast<unsigned long long>(*vptrA2) << std::endl; ///指定同一个地址
+        /// ***虚指针不一样 但是指向同一个虚表
     }
     getchar();
     return 0;
