68e39d6720c530fb8e77f25a1b3d2095c4a4b942
handabao <18862963156@163.com>
2025-10-16 09:21:53 +0800

    41完成XData使用内存池管理数据内存

---
+++ src/memory_XFile_SSL/include/XData.h
@@ -0,0 +1,47 @@
+﻿/**
+ * @file   XData.h
+ * @brief  
+ *
+ * @details   
+ *
+ * @author 31667
+ * @date   2025-10-15
+ */
+
+#ifndef XDATA_H
+#define XDATA_H
+
+#include <memory>
+
+namespace std::pmr
+{
+    class memory_resource;
+}
+
+
+class XData
+{
+public:
+    using Ptr          = std::shared_ptr<XData>;
+    using SmartMemPool = std::shared_ptr<std::pmr::memory_resource>;
+
+public:
+    static auto create(const SmartMemPool &pool) -> XData::Ptr;
+
+    /// \brief
+    /// \param mem_size  占用内存字节数
+    /// \return
+    auto allocate(long long mem_size) -> void *;
+
+private:
+    XData();
+    XData(const SmartMemPool &pool);
+    virtual ~XData();
+
+private:
+    class PImpl;
+    std::unique_ptr<PImpl> impl_;
+};
+
+
+#endif // XDATA_H
---
+++ src/memory_XFile_SSL/src/XData.cpp
@@ -0,0 +1,73 @@
+﻿#include "XData.h"
+
+#include <iostream>
+
+class XData::PImpl
+{
+public:
+    PImpl(XData *owenr);
+    PImpl(XData *owenr, SmartMemPool pool);
+    ~PImpl() = default;
+
+public:
+    XData       *owenr_ = nullptr;
+    SmartMemPool mem_pool_;
+    long long    mem_size_ = 0; ///< 占用空间字节数
+    long long    size_     = 0; ///< 数据字节数
+    void        *data_     = nullptr;
+};
+
+XData::PImpl::PImpl(XData *owenr) : owenr_(owenr)
+{
+}
+
+XData::PImpl::PImpl(XData *owenr, SmartMemPool pool) : owenr_(owenr), mem_pool_(std::move(pool))
+{
+}
+
+XData::XData()
+{
+    impl_ = std::make_unique<XData::PImpl>(this);
+}
+
+XData::XData(const SmartMemPool &pool)
+{
+    impl_ = std::make_unique<XData::PImpl>(this, pool);
+}
+
+XData::~XData()
+{
+    if (!impl_->data_ || !impl_->mem_pool_)
+    {
+        return;
+    }
+
+    impl_->mem_pool_->deallocate(impl_->data_, impl_->mem_size_);
+    impl_->data_     = nullptr;
+    impl_->mem_size_ = 0;
+    impl_->size_     = 0;
+}
+
+auto XData::create(const SmartMemPool &pool) -> XData::Ptr
+{
+    return std::make_shared<XData>(pool);
+}
+
+auto XData::allocate(long long mem_size) -> void *
+{
+    if (mem_size <= 0)
+    {
+        std::cerr << "XData::allocate failed! mem_size<=0" << std::endl;
+        return nullptr;
+    }
+    if (!impl_->mem_pool_)
+    {
+        std::cerr << "XData::allocate failed! mem_pool_ is null!" << std::endl;
+        return nullptr;
+    }
+
+    impl_->data_     = impl_->mem_pool_->allocate(mem_size);
+    impl_->mem_size_ = mem_size;
+    impl_->size_     = mem_size;
+    return impl_->data_;
+}
